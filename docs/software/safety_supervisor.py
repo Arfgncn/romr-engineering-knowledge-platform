5118352ff59f66e1743848df591827fcace6d149
Arfgncn <99676636+Arfgncn@users.noreply.github.com>
2026-06-16 12:21:39 +0300

    Update safety_supervisor.py

--- docs/software/safety_supervisor.py
+++ docs/software/safety_supervisor.py
@@ -14,6 +14,9 @@ class SafetyState:
     gun_temperature_ok: bool
     pressure_ok: bool
     fire_alarm_ok: bool
+    service_covers_closed: bool = True
+    generator_ok: bool = True
+    suction_ok: bool = True
 
 
 class SafetySupervisor:
@@ -24,4 +27,27 @@ class SafetySupervisor:
             state.gun_temperature_ok,
             state.pressure_ok,
             state.fire_alarm_ok,
+            state.service_covers_closed,
+            state.generator_ok,
+            state.suction_ok,
         ])
+
+    def alarm_list(self, state: SafetyState) -> list[str]:
+        alarms = []
+        if not state.e_stop_ok:
+            alarms.append("E_STOP_ACTIVE")
+        if not state.robot_safe:
+            alarms.append("ROBOT_SAFETY_ZONE")
+        if not state.gun_temperature_ok:
+            alarms.append("GUN_OVERTEMPERATURE")
+        if not state.pressure_ok:
+            alarms.append("PAINT_PRESSURE_ALARM")
+        if not state.fire_alarm_ok:
+            alarms.append("FIRE_ALARM")
+        if not state.service_covers_closed:
+            alarms.append("SERVICE_COVER_OPEN")
+        if not state.generator_ok:
+            alarms.append("GENERATOR_ALARM")
+        if not state.suction_ok:
+            alarms.append("SUCTION_FAILURE")
+        return alarms
